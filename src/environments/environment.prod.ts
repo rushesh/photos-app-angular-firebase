1d38ab6c81ce6e520978dec121420961e041d02a
RUSHESH SHARMA <sharmarushesh@gmail.com>
2020-03-31 04:50:42 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,36 +1,49 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyCBri1uXVayTJEzVT9H3sfQl3SQO6oAd6o",
-    authDomain: "angular-image-gallery-6e84c.firebaseapp.com",
-    databaseURL: "https://angular-image-gallery-6e84c.firebaseio.com",
-    projectId: "angular-image-gallery-6e84c",
-    storageBucket: "angular-image-gallery-6e84c.appspot.com",
-    messagingSenderId: "830419863234",
-    appId: "1:830419863234:web:0f030b1003de17a78d1d5b",
-    measurementId: "G-PX1B6SXSH3"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   },
   loginDB:{
     cred1admin:{
-      id:'admin123@photosapp.org',
-      password:'Admin1@123',
-      name:'admin1'
+      id:'',
+      password:'',
+      name:''
     },
     cred2admin:{
-      id:'admin@photosapp.org',
-      password:'Admin2@123',
-      name:'admin2'
+      id:'',
+      password:'',
+      name:''
     },
     cred1test:{
-      id:'test123@photosapp.org',
-      password:'Test1@123',
-      name:'tester1'
+      id:'',
+      password:'',
+      name:''
     },
     cred2test:{
-      id:'test@photosapp.org',
-      password:'Test2@123',
-      name:'tester2'
+      id:'',
+      password:'',
+      name:''
     }
   },
   timerLogout:100
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
